7d1b2d95f8836aa7e8333f6bc0129ba277b0bf60
vyaparkarahasya <nwbsaqib@gmail.com>
2025-12-26 01:29:29 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -224,7 +224,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/TechyUpdateHQ">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/https://t.me/TechyUpdateHQ">[ Muft Deals ]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/muftDeals">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/TechyUpdateHQ">[ Techy Update]</a></b>"""
 
     
     MOVIE_UPDATE_NOTIFY_TXT = """
