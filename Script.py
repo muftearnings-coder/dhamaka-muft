ecebcdaebaee9807746974ae4e22b073c586d7c5
vyaparkarahasya <nwbsaqib@gmail.com>
2025-12-29 04:23:34 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -224,7 +224,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/muftDeals">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/TechyUpdateHQ">[ Techy Update]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/TechyUpdateHQ">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/hindimovies_indian">[ Hindi Movies ]</a></b>"""
 
     
     MOVIE_UPDATE_NOTIFY_TXT = """
